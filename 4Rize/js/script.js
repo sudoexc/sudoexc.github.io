29200e44a84c694b711df0adf7d5b69e3de28846
Joe <javokhir.369@gmail.com>
2024-05-05 01:31:27 +0500

    4rize

--- 4Rize/js/script.js
+++ 4Rize/js/script.js
@@ -1,1 +1,28 @@
-'use strict'
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+const filterBtn = document.querySelector('.toggleButton'),
+    menu = document.querySelector('.tablet'),
+    closeElem = document.querySelector('.close'),
+    overlay = document.querySelector('.overlay');
+
+filterBtn.addEventListener('click', () => {
+    menu.classList.add('active');
+    document.body.style.overflow = 'hidden';
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+    document.body.style.overflow = '';
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    document.body.style.overflow = '';
+});
+
+overlay.addEventListener('touchstart', () => {
+    menu.classList.remove('active');
+    document.body.style.overflow = '';
+});
+});
