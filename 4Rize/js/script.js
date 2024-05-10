26aa86bc259b0087b4c2605dd1507a09e489f9d6
Joe <javokhir.369@gmail.com>
2024-05-11 02:42:57 +0500

    11 05

--- 4Rize/js/script.js
+++ 4Rize/js/script.js
@@ -4,25 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
 const filterBtn = document.querySelector('.toggleButton'),
     menu = document.querySelector('.tablet'),
     closeElem = document.querySelector('.close'),
-    overlay = document.querySelector('.overlay');
+    overlay = document.querySelector('.overlay'),
+    mobBtn = document.querySelector('.phone-btn'),
+    mobBtn2 = document.querySelector('.white-btn'),
+    tabBtn = document.querySelector('.tablet-btn'),
+    home = document.querySelector('.main');
 
 filterBtn.addEventListener('click', () => {
     menu.classList.add('active');
-    document.body.style.overflow = 'hidden';
+    home.style.display = 'none';
 });
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
-    document.body.style.overflow = '';
+    home.style.display = '';
 });
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
-    document.body.style.overflow = '';
+    home.style.display = '';
+});
+
+mobBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
+    home.style.display = '';
+});
+
+mobBtn2.addEventListener('click', () => {
+    menu.classList.remove('active');
+    home.style.display = '';
+});
+
+tabBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
+    home.style.display = '';
 });
 
 overlay.addEventListener('touchstart', () => {
     menu.classList.remove('active');
-    document.body.style.overflow = '';
+    home.style.display = '';
 });
 });
